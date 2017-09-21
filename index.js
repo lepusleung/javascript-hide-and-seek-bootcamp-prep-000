fdd5fc6e9bf2a3816d1734d3695132a075bac35e
Man Leung <lepusleung@gmail.com>
2017-09-21 14:53:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
